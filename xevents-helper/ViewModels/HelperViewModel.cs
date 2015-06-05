3473f0aa3173a14f98a77d2f92e38e2081842685
Thomas Stringer <sqlsalt@outlook.com>
2015-06-05 17:45:45 -0400

    WIP: Adding elements to main view

--- xevents-helper/ViewModels/HelperViewModel.cs
+++ xevents-helper/ViewModels/HelperViewModel.cs
@@ -11,5 +11,6 @@ namespace xevents_helper.ViewModels
     {
         public IEnumerable<SelectListItem> Releases { get; set; }
         public string SelectedRelease { get; set; }
+        public string SessionName { get; set; }
     }
 }
