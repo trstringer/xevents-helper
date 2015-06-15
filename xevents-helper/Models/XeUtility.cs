2b57911e46c9f68923c264be8ffe6d72292aeeac
Thomas Stringer <sqlsalt@outlook.com>
2015-06-15 14:30:20 -0400

    Created barebones utility class to generate the T-SQL

---
+++ xevents-helper/Models/XeUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xevents_helper.Models
+{
+    public class XeUtility
+    {
+        public string GetCreateDdl(XeSession session)
+        {
+            string sessionDefinition = GetCreateEventSessionClause(session.Name);
+
+            sessionDefinition += ";\r\nGO";
+
+            return sessionDefinition;
+        }
+
+        private string GetCreateEventSessionClause(string sessionName)
+        {
+            return string.Format("CREATE EVENT SESSION {0}\r\nON SERVER", QuoteName(sessionName));
+        }
+
+        private string QuoteName(string name)
+        {
+            return string.Format("[{0}]", name);
+        }
+    }
+}
