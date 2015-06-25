f01e8bc5dbb0cd102503a915f4e94d581a930240
Thomas Stringer <sqlsalt@outlook.com>
2015-06-25 17:01:11 -0400

    WIP: Added basic add event logic for the CREATE SESSION definition

--- xevents-helper/Models/XeUtility.cs
+++ xevents-helper/Models/XeUtility.cs
@@ -9,16 +9,23 @@ namespace xevents_helper.Models
     {
         public string GetCreateDdl(Session session)
         {
-            string sessionDefinition = GetCreateEventSessionClause(session.Name);
+            string sessionDefinition = GetCreateEventSessionClause(session);
 
             sessionDefinition += ";\r\nGO";
 
             return sessionDefinition;
         }
 
-        private string GetCreateEventSessionClause(string sessionName)
+        private string GetCreateEventSessionClause(Session session)
         {
-            return string.Format("CREATE EVENT SESSION {0}\r\nON SERVER", QuoteName(sessionName));
+            return string.Format("CREATE EVENT SESSION {0}\r\nON SERVER", QuoteName(session.Name));
+        }
+
+        private string GetAddEventClause(Event xeEvent)
+        {
+            string addEventClause = string.Format("ADD EVENT {0}.{1}", QuoteName(xeEvent.PackageName), QuoteName(xeEvent.Name));
+            
+            return addEventClause;
         }
 
         private string QuoteName(string name)
