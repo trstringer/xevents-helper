212b9fe49e637c708db6e109e074ab4dd4561667
Thomas Stringer <sqlsalt@outlook.com>
2015-07-06 17:43:57 -0400

    Coupled the release with the session

--- xevents-helper/Models/Session.cs
+++ xevents-helper/Models/Session.cs
@@ -10,5 +10,6 @@ namespace xevents_helper.Models
         public string Name { get; set; }
         public IEnumerable<Event> Events { get; set; }
         public IEnumerable<Target> Targets { get; set; }
+        public Release TargetRelease { get; set; }
     }
 }
