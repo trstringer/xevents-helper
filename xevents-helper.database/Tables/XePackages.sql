716744fdc304cc80c938268f37a55e2e8d41c5a5
Thomas Stringer <sqlsalt@outlook.com>
2015-07-07 09:48:09 -0400

    Added packages table

---
+++ xevents-helper.database/Tables/XePackages.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[XePackages]
+(
+    ReleaseId int not null 
+        constraint FK_XePackages_Release_Id foreign key references dbo.Release(Id), 
+    [name] NVARCHAR(60) NOT NULL, 
+    [guid] UNIQUEIDENTIFIER NOT NULL, 
+    [description] NVARCHAR(256) NOT NULL, 
+    [capabilities] INT NULL, 
+    [capabilities_desc] NVARCHAR(256) NULL, 
+    [module_guid] UNIQUEIDENTIFIER NOT NULL, 
+    [module_address] VARBINARY(8) NOT NULL,
+    constraint PK_XePackages_NameReleaseId primary key (name, ReleaseId, [guid])
+);
