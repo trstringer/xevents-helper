808b607f0340b337e397b0b441e773f095f43b34
Thomas Stringer <sqlsalt@outlook.com>
2015-07-07 15:09:26 -0400

    Refactored the event field class

--- xevents-helper/Models/EventField.cs
+++ xevents-helper/Models/EventField.cs
@@ -8,6 +8,8 @@ namespace xevents_helper.Models
     public class EventField
     {
         public string Name { get; set; }
+        public string TypeName { get; set; }
         public bool IsOptional { get; set; }
+        public string Description { get; set; }
     }
 }
