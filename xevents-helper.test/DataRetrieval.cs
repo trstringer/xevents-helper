5734392b594ee4b0ec479971c623378fa6a282c3
Thomas Stringer <sqlsalt@outlook.com>
2015-07-09 17:42:11 -0400

    Added unit test for event retrieval

--- xevents-helper.test/DataRetrieval.cs
+++ xevents-helper.test/DataRetrieval.cs
@@ -2,18 +2,41 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using xevents_helper;
 using xevents_helper.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace xevents_helper.test
 {
     [TestClass]
     public class DataRetrieval
     {
+        private DataGatherer _dataGatherer;
+        private string _testReleaseName;
+        private Release _testRelease;
+
+        public DataRetrieval()
+        {
+            _dataGatherer = new DataGatherer();
+            _testReleaseName = "SQL 2012";
+            _testRelease = new Release() { Name = _testReleaseName };
+        }
+
         [TestMethod]
         public void DataGathererInstantiation()
         {
             DataGatherer dg = new DataGatherer();
 
             Assert.IsNotNull(dg);
+        }
+
+        [TestMethod]
+        public void EventsRetrieval()
+        {
+            IEnumerable<Event> events = _dataGatherer.GetAllEventsForRelease(_testRelease);
+
+            Assert.IsNotNull(events);
+            Assert.IsTrue(events.ToList().Count > 0);
         }
     }
 }
