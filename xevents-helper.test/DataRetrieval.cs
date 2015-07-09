b9276f983c4a6a174dabea6cef35f8a03fd6cb7a
Thomas Stringer <sqlsalt@outlook.com>
2015-07-09 17:27:24 -0400

    Added unit tests

---
+++ xevents-helper.test/DataRetrieval.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xevents_helper;
+using xevents_helper.Models;
+
+namespace xevents_helper.test
+{
+    [TestClass]
+    public class DataRetrieval
+    {
+        [TestMethod]
+        public void DataGathererInstantiation()
+        {
+            DataGatherer dg = new DataGatherer();
+
+            Assert.IsNotNull(dg);
+        }
+    }
+}
