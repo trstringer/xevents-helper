29f58fe13ce48159c7844e7541f57f09fffee56c
Thomas Stringer <sqlsalt@outlook.com>
2015-07-13 11:04:46 -0400

    Changed event search to return package name as well

--- xevents-helper.database/StoredProcedures/EventSearchByName.sql
+++ xevents-helper.database/StoredProcedures/EventSearchByName.sql
@@ -2,10 +2,16 @@
     @ReleaseName varchar(32),
     @SearchString varchar(512) = null
 AS
-    select o.name, o.description
+    select 
+		o.name, 
+		package_name = p.name,
+		o.description
     from dbo.XeObjects o
     inner join dbo.Release r
     on o.ReleaseId = r.Id
+	inner join dbo.XePackages p
+	on o.package_guid = p.guid
+	and o.ReleaseId = p.ReleaseId
     where r.FriendlyName= @ReleaseName
     and o.object_type = 'event'
     and 
@@ -13,5 +19,5 @@ AS
         @SearchString is null
         or o.name like '%' + @SearchString + '%'
     )
-    order by name;
+    order by o.name;
 go
