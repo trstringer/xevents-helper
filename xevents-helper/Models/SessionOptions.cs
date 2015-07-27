8f4216b33759c2a93b23b5bb1a56db7317bb8de4
Thomas Stringer <sqlsalt@outlook.com>
2015-07-27 13:42:37 -0400

    Added framework for session options and their specification

---
+++ xevents-helper/Models/SessionOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xevents_helper.Models
+{
+    public class SessionOptions
+    {
+        public int MaxMemory { get; set; }
+        public EventRetentionMode EventRetentionMode { get; set; }
+        public int MaxDispatchLatency { get; set; }
+        public int MaxEventSize { get; set; }
+        public MemoryPartitionMode MemoryPartitionMode { get; set; }
+        public bool TrackCausality { get; set; }
+        public bool StartWithInstance { get; set; }
+    }
+}
