991c921640122cf7065e0980ad38785629b21e1e
Thomas Stringer <sqlsalt@outlook.com>
2015-08-15 16:35:03 -0400

    Refactored table hover logic

--- xevents-helper/Scripts/site.js
+++ xevents-helper/Scripts/site.js
@@ -1,10 +1,5 @@
 ﻿function setEvents() {
-    $("#eventSearchResults td").hover(function () {
-        $(this).addClass("active");
-    });
-    $("#eventSearchResults td").mouseleave(function () {
-        $(this).removeClass("active");
-    });
+    
 }
 
 $(function () {
