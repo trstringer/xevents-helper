e8ae7b8bb67f7a882d66e40d20856d4174315920
Thomas Stringer <sqlsalt@outlook.com>
2015-08-21 10:01:39 -0400

    WIP: Changing logic to the owning td for the click event

--- xevents-helper/Scripts/site.js
+++ xevents-helper/Scripts/site.js
@@ -25,6 +25,10 @@ function setEvents() {
         //
         retrieveEventDescription($(this).text().trim());
     });
+
+    $(".event-add-clicker").on("click", function () {
+        alert($(this).attr("data-eventname"));
+    });
 }
 
 function getReleaseName() {
@@ -57,7 +61,7 @@ function clearEventSearchInput() {
 }
 function addEventSearchResult(eventName) {
     $("#eventSearchResults").append(
-        '<tr><td>' + eventName + '</td><td><span data-eventname="' + eventName + '" class="glyphicon glyphicon-plus"></span></td></tr>');
+        '<tr><td>' + eventName + '</td><td data-eventname="' + eventName + '" class="event-add-clicker"><span class="glyphicon glyphicon-plus"></span></td></tr>');
 
     $("#eventSearchResults span").hide();
 }
